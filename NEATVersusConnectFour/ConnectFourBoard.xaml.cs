b3ea7b3e783834b936f24604f5ccd66c67e2a288
Roland Munsil <roland@munsil.com>
2017-03-01 22:36:36 -0600

    Moved connect four board into a separate control

---
+++ NEATVersusConnectFour/ConnectFourBoard.xaml.cs
@@ -0,0 +1,226 @@
+﻿using SharpNeat.Phenomes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NEATVersusConnectFour
+{
+    /// <summary>
+    /// Interaction logic for ConnectFourBoard.xaml
+    /// </summary>
+    public partial class ConnectFourBoard : UserControl
+    {
+        double discSize = 50;
+        double discSpacing = 10;
+        double triangleHeight = 30;
+        Vector boardTopLeft = new Vector(100, 150);
+        Dictionary<object, int> polygonCols;
+
+        TextBlock winText;
+
+        Board board;
+        double curDiscColor;
+        Ellipse[,] discs;
+
+        bool playingGame = false;
+
+        public ConnectFourBoard()
+        {
+            InitializeComponent();
+
+            polygonCols = new Dictionary<object, int>();
+
+            for (int c = 0; c < Board.NUM_COLS; c++)
+            {
+                Polygon polygon = new Polygon();
+                polygon.Fill = Brushes.SlateGray;
+                polygon.StrokeThickness = 0;
+                polygon.HorizontalAlignment = HorizontalAlignment.Left;
+                polygon.VerticalAlignment = VerticalAlignment.Top;
+                polygon.Points = new PointCollection()
+                {
+                    boardTopLeft + new Point(             c * (discSize + discSpacing), 0),
+                    boardTopLeft + new Point(discSize +   c * (discSize + discSpacing), 0),
+                    boardTopLeft + new Point(discSize/2 + c * (discSize + discSpacing), triangleHeight),
+                };
+
+                polygonCols.Add(polygon, c);
+                polygon.MouseUp += Triangle_MouseDown;
+                grid.Children.Add(polygon);
+            }
+
+            discs = new Ellipse[Board.NUM_ROWS, Board.NUM_COLS];
+
+            //Construct circles
+            for (int r = 0; r < Board.NUM_ROWS; r++)
+            {
+                for (int c = 0; c < Board.NUM_COLS; c++)
+                {
+                    Ellipse disc = new Ellipse();
+                    disc.Height = discSize;
+                    disc.Width = discSize;
+                    disc.Fill = Brushes.LightGray;
+                    disc.StrokeThickness = 0;
+                    disc.HorizontalAlignment = HorizontalAlignment.Left;
+                    disc.VerticalAlignment = VerticalAlignment.Top;
+                    disc.Margin = new Thickness(boardTopLeft.X + c * (discSize + discSpacing),
+                                                boardTopLeft.Y + triangleHeight + discSpacing + r * (discSize + discSpacing),
+                                                0, 0);
+
+                    grid.Children.Add(disc);
+                    discs[r, c] = disc;
+                }
+            }
+
+            winText = new TextBlock();
+            winText.Text = "";
+            winText.TextAlignment = TextAlignment.Center;
+            winText.HorizontalAlignment = HorizontalAlignment.Left;
+            winText.VerticalAlignment = VerticalAlignment.Top;
+            winText.FontSize = 48;
+
+            double boardWidth = (discSize * Board.NUM_COLS) + (discSpacing * (Board.NUM_COLS - 1));
+            double boardHeight = (discSize * Board.NUM_ROWS) + (discSpacing * (Board.NUM_ROWS - 1))
+                                 + triangleHeight + discSpacing;
+
+            winText.Width = boardWidth;
+            winText.Margin = new Thickness(boardTopLeft.X, boardTopLeft.Y + boardHeight / 2, 0, 0);
+
+            grid.Children.Add(winText);
+        }
+
+        public void BeginGame()
+        {
+            ResetUI();
+            board = new Board();
+            curDiscColor = Board.YELLOW_DISC;
+            playingGame = true;
+        }
+
+        private void ResetUI()
+        {
+            for (int r = 0; r < Board.NUM_ROWS; r++)
+            {
+                for (int c = 0; c < Board.NUM_COLS; c++)
+                {
+                    discs[r, c].Fill = Brushes.LightGray;
+                }
+            }
+
+            winText.Text = "";
+        }
+
+        private void Triangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (playingGame)
+            {
+                int col = polygonCols[sender];
+                if (board.CanAddDisc(col))
+                {
+                    int row = board.RowWhereDiscWillBeAdded(col);
+                    double winner = board.AddDisc(curDiscColor, col);
+                    discs[row, col].Fill = curDiscColor == Board.YELLOW_DISC ? Brushes.Yellow : Brushes.Red;
+
+                    if (winner != 0)
+                    {
+                        playingGame = false;
+                        winText.Text = winner == Board.YELLOW_DISC ? "Yellow wins!" : "Red wins!";
+                        new Thread(delegate ()
+                        {
+                            Thread.Sleep(1000);
+                            grid.Dispatcher.Invoke(BeginGame);
+
+                        }).Start();
+                        return;
+                    }
+
+                    curDiscColor = -curDiscColor;
+                }
+            }
+        }
+
+        public void VisualizeGame(IBlackBox yellowPlayer, IBlackBox redPlayer)
+        {
+            //One of them can't play
+            if (yellowPlayer == null || redPlayer == null)
+                throw new ArgumentException();
+
+            board = new Board();
+            curDiscColor = Board.YELLOW_DISC;
+            grid.Dispatcher.Invoke(ResetUI);
+
+            int curTurn = 0;
+            while (true)
+            {
+                if (curTurn == (Board.NUM_ROWS * Board.NUM_COLS))
+                {
+                    //No more pieces to place - no one wins!
+                    winText.Dispatcher.Invoke((Action)delegate ()
+                    {
+                        winText.Text = "Tie!";
+                    });
+                    return;
+                }
+
+                IBlackBox curPlayer = curDiscColor == Board.YELLOW_DISC ? yellowPlayer : redPlayer;
+
+                int colToAddAt = -1;
+
+                double[] outputs = new double[curPlayer.OutputCount];
+                curPlayer.OutputSignalArray.CopyTo(outputs, 0);
+                IEnumerable<int> orderedCols = outputs.Select((a, i) => Tuple.Create(a, i)).OrderByDescending(t => t.Item1).Select(t => t.Item2);
+                foreach (int col in orderedCols)
+                {
+                    if (board.CanAddDisc(col))
+                    {
+                        colToAddAt = col;
+                        break;
+                    }
+                }
+
+                //if (!board.CanAddDisc(colToAddAt))
+                //{
+                //    //Whoever is playing has lost
+                //    winText.Dispatcher.Invoke((Action)delegate ()
+                //    {
+                //        winText.Text = curDiscColor == Board.YELLOW_DISC ?
+                //            $"Red won (because yellow tried to drop in column {maxCol}" :
+                //            $"Yellow won (because red tried to drop in column {maxCol}";
+                //    });
+                //    return;
+                //}
+
+                int row = board.RowWhereDiscWillBeAdded(colToAddAt);
+                double winner = board.AddDisc(curDiscColor, colToAddAt);
+                grid.Dispatcher.Invoke(delegate ()
+                {
+                    discs[row, colToAddAt].Fill = curDiscColor == Board.YELLOW_DISC ? Brushes.Yellow : Brushes.Red;
+                });
+
+                if (winner != 0)
+                {
+                    winText.Dispatcher.Invoke((Action)delegate ()
+                    {
+                        winText.Text = curDiscColor == Board.YELLOW_DISC ? $"Yellow won!" : $"Red won!";
+                    });
+                    return;
+                }
+                Thread.Sleep(500);
+                curDiscColor = -curDiscColor;
+                curTurn++;
+            }
+        }
+    }
+}
--- NEATVersusConnectFour/MainWindow.xaml.cs
+++ NEATVersusConnectFour/MainWindow.xaml.cs
@@ -53,8 +53,6 @@ namespace NEATVersusConnectFour
         {
             //TODO: try HyperNEAT - it seems like it would work better for this situation
 
-            mainGrid.Dispatcher.Invoke(CreateBoardGUI);
-
             doneEvaluatingGenerationBarrier = new Barrier(2, (b)=>ShowStats());
 
             doneShowingStatsEvents = new Dictionary<double, AutoResetEvent>()
@@ -168,7 +166,7 @@ namespace NEATVersusConnectFour
 
             if (redTeam.CurrentGeneration % 20 == 0)
             {
-                VisualizeGame((IBlackBox)yellowTeam.CurrentChampGenome.CachedPhenome, (IBlackBox)redTeam.CurrentChampGenome.CachedPhenome);
+                cfBoard.VisualizeGame((IBlackBox)yellowTeam.CurrentChampGenome.CachedPhenome, (IBlackBox)redTeam.CurrentChampGenome.CachedPhenome);
                 Thread.Sleep(1000);
             }
 
@@ -188,199 +186,9 @@ namespace NEATVersusConnectFour
             textBlock.Text += line + "\r\n";
         }
 
-        private void VisualizeGame(IBlackBox yellowPlayer, IBlackBox redPlayer)
-        {
-            //One of them can't play
-            if (yellowPlayer == null || redPlayer == null)
-                throw new ArgumentException();
-
-            board = new Board();
-            curDiscColor = Board.YELLOW_DISC;
-            mainGrid.Dispatcher.Invoke(ResetBoardGUI);
-
-            int curTurn = 0;
-            while (true)
-            {
-                if (curTurn == (Board.NUM_ROWS * Board.NUM_COLS))
-                {
-                    //No more pieces to place - no one wins!
-                    winText.Dispatcher.Invoke((Action)delegate ()
-                    {
-                        winText.Text = "Tie!";
-                    });
-                    return;
-                }
-
-                IBlackBox curPlayer = curDiscColor == Board.YELLOW_DISC ? yellowPlayer : redPlayer;
-
-                int colToAddAt = -1;
-
-                double[] outputs = new double[curPlayer.OutputCount];
-                curPlayer.OutputSignalArray.CopyTo(outputs, 0);
-                IEnumerable<int> orderedCols = outputs.Select((a, i) => Tuple.Create(a, i)).OrderByDescending(t => t.Item1).Select(t => t.Item2);
-                foreach (int col in orderedCols)
-                {
-                    if (board.CanAddDisc(col))
-                    {
-                        colToAddAt = col;
-                        break;
-                    }
-                }
-
-                //if (!board.CanAddDisc(colToAddAt))
-                //{
-                //    //Whoever is playing has lost
-                //    winText.Dispatcher.Invoke((Action)delegate ()
-                //    {
-                //        winText.Text = curDiscColor == Board.YELLOW_DISC ?
-                //            $"Red won (because yellow tried to drop in column {maxCol}" :
-                //            $"Yellow won (because red tried to drop in column {maxCol}";
-                //    });
-                //    return;
-                //}
-
-                int row = board.RowWhereDiscWillBeAdded(colToAddAt);
-                double winner = board.AddDisc(curDiscColor, colToAddAt);
-                mainGrid.Dispatcher.Invoke(delegate ()
-                {
-                    discs[row, colToAddAt].Fill = curDiscColor == Board.YELLOW_DISC ? Brushes.Yellow : Brushes.Red;
-                });
-
-                if (winner != 0)
-                {
-                    winText.Dispatcher.Invoke((Action)delegate ()
-                    {
-                        winText.Text = curDiscColor == Board.YELLOW_DISC ? $"Yellow won!" : $"Red won!";
-                    });
-                    return;
-                }
-                Thread.Sleep(500);
-                curDiscColor = -curDiscColor;
-                curTurn++;
-            }
-        }
-
-        double discSize = 50;
-        double discSpacing = 10;
-        double triangleHeight = 30;
-        Vector boardTopLeft = new Vector(100, 150);
-        Dictionary<object, int> polygonCols;
-
-        TextBlock winText;
-
-        Board board;
-        double curDiscColor;
-        Ellipse[,] discs;
-
         private void playGameButton_Click(object sender, RoutedEventArgs e)
         {
-            CreateBoardGUI();
-
-            board = new Board();
-            curDiscColor = Board.YELLOW_DISC;
-        }
-
-        private void CreateBoardGUI()
-        {
-            polygonCols = new Dictionary<object, int>();
-
-            for (int c = 0; c < Board.NUM_COLS; c++)
-            {
-                Polygon polygon = new Polygon();
-                polygon.Fill = Brushes.SlateGray;
-                polygon.StrokeThickness = 0;
-                polygon.HorizontalAlignment = HorizontalAlignment.Left;
-                polygon.VerticalAlignment = VerticalAlignment.Top;
-                polygon.Points = new PointCollection()
-                {
-                    boardTopLeft + new Point(             c * (discSize + discSpacing), 0),
-                    boardTopLeft + new Point(discSize +   c * (discSize + discSpacing), 0),
-                    boardTopLeft + new Point(discSize/2 + c * (discSize + discSpacing), triangleHeight),
-                };
-
-                polygonCols.Add(polygon, c);
-                polygon.MouseUp += Triangle_MouseDown;
-                mainGrid.Children.Add(polygon);
-            }
-
-            discs = new Ellipse[Board.NUM_ROWS, Board.NUM_COLS];
-
-            //Construct circles
-            for (int r = 0; r < Board.NUM_ROWS; r++)
-            {
-                for (int c = 0; c < Board.NUM_COLS; c++)
-                {
-                    Ellipse disc = new Ellipse();
-                    disc.Height = discSize;
-                    disc.Width = discSize;
-                    disc.Fill = Brushes.LightGray;
-                    disc.StrokeThickness = 0;
-                    disc.HorizontalAlignment = HorizontalAlignment.Left;
-                    disc.VerticalAlignment = VerticalAlignment.Top;
-                    disc.Margin = new Thickness(boardTopLeft.X + c * (discSize + discSpacing),
-                                                boardTopLeft.Y + triangleHeight + discSpacing + r * (discSize + discSpacing),
-                                                0, 0);
-
-                    mainGrid.Children.Add(disc);
-                    discs[r, c] = disc;
-                }
-            }
-
-            winText = new TextBlock();
-            winText.Text = "";
-            winText.TextAlignment = TextAlignment.Center;
-            winText.HorizontalAlignment = HorizontalAlignment.Left;
-            winText.VerticalAlignment = VerticalAlignment.Top;
-            winText.FontSize = 48;
-
-            double boardWidth = (discSize * Board.NUM_COLS) + (discSpacing * (Board.NUM_COLS - 1));
-            double boardHeight = (discSize * Board.NUM_ROWS) + (discSpacing * (Board.NUM_ROWS - 1))
-                                 + triangleHeight + discSpacing;
-
-            winText.Width = boardWidth;
-            winText.Margin = new Thickness(boardTopLeft.X, boardTopLeft.Y + boardHeight / 2, 0, 0);
-
-            mainGrid.Children.Add(winText);
-        }
-
-        private void ResetBoardGUI()
-        {
-            for (int r = 0; r < Board.NUM_ROWS; r++)
-            {
-                for (int c = 0; c < Board.NUM_COLS; c++)
-                {
-                    discs[r, c].Fill = Brushes.LightGray;
-                }
-            }
-
-            winText.Text = "";
-        }
-
-        private void Triangle_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            int col = polygonCols[sender];
-            if (board.CanAddDisc(col))
-            {
-                int row = board.RowWhereDiscWillBeAdded(col);
-                double winner = board.AddDisc(curDiscColor, col);
-                discs[row, col].Fill = curDiscColor == Board.YELLOW_DISC ? Brushes.Yellow : Brushes.Red;
-
-                if (winner != 0)
-                {
-                    winText.Text = winner == Board.YELLOW_DISC ? "Yellow wins!" : "Red wins!";
-                    new Thread(delegate()
-                    {
-                        Thread.Sleep(1000);
-                        mainGrid.Dispatcher.BeginInvoke((Action)ResetBoardGUI);
-                        board = new Board();
-                        curDiscColor = Board.YELLOW_DISC;
-
-                    }).Start();
-                    return;
-                }
-
-                curDiscColor = -curDiscColor;
-            }
+            cfBoard.BeginGame();
         }
     }
 }
